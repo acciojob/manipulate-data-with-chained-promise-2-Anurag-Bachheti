516815466b7bc0a36137daddbd8715f685509293
Anurag Bachheti <157354714+Anurag-Bachheti@users.noreply.github.com>
2025-04-18 00:25:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,12 @@
 let arrNums = [1, 2, 3, 4];
 let btn = document.getElementById("btn");
 
-btn.addEventListener("click", myFunc);
+btn.addEventListener("click", evenFunc);
 
-function myFunc() {
-  // document.getElementById("output").innerHTML = "Processing...";
-
+function evenFunc(){
+  let evens = arrNums.filter(num => num % 2 === 0);
   setTimeout(() => {
-    document.getElementById("output").innerHTML = arrNums.join(", ");
-    evenFunc(arrNums);
-  }, 3000);
-}
-
-function evenFunc(arr) {
-  let evens = arr.filter(num => num % 2 === 0);
-
-  setTimeout(() => {
-    document.getElementById("output").innerHTML += `<br>${evens.join(", ")}`;
-    
-    // Double the even numbers after 2s
+    document.getElementById("output").innerHTML += `${evens.join(",")}`;
     doubleEvens(evens);
   }, 1000);
 }
@@ -27,6 +15,6 @@ function doubleEvens(evenArr) {
   let doubled = evenArr.map(num => num * 2);
 
   setTimeout(() => {
-    document.getElementById("output").innerHTML += `<br>${doubled.join(", ")}`;
+    document.getElementById("output").innerHTML += `<br>${doubled.join(",")}`;
   }, 2000);
 }
