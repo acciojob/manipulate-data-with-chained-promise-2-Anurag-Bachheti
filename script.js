355fd64108ea1bf54c5550c3ee3d807179751d05
Anurag Bachheti <157354714+Anurag-Bachheti@users.noreply.github.com>
2025-04-17 17:11:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//your JS code here. If required.
+// JavaScript
+let arrNums = [1, 2, 3, 4];
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click", myFunc);
+
+function myFunc() {
+  setTimeout(() => {
+    document.getElementById("output").innerHTML = arrNums.join(", ");
+    
+    // Now call evenFunc after showing original array
+    evenFunc(arrNums);
+  }, 3000);
+}
+
+function evenFunc(arr) {
+  let latestArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      latestArr.push(arr[i]);
+    }
+  }
+  setTimeout(() => {
+    document.getElementById("output").innerHTML = latestArr.join(", ");
+  }, 1000); // Show even numbers after 1 second
+}
